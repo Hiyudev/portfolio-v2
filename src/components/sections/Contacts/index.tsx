87758467f9a8882279a0319002b47aac917aa33a
Hiyudev <37001732+Hiyudev@users.noreply.github.com>
2022-07-12 20:07:20 -0300

    Added contact image

--- src/components/sections/Contacts/index.tsx
+++ src/components/sections/Contacts/index.tsx
@@ -1,5 +1,6 @@
 import { useTranslation } from "next-i18next";
 import Title from "../../common/Title";
+import Image from "next/image";
 
 interface ContactsSectionProps {
   description: string;
@@ -9,10 +10,20 @@ function ContactsSection({ description }: ContactsSectionProps) {
   const { t } = useTranslation("home");
 
   return (
-    <section className="mt-8 grid grid-cols-1 gap-4 md:mt-16 md:grid-cols-2">
+    <section className="mt-8 grid grid-rows-2 gap-4 md:mt-16 md:grid-cols-2">
       <div className="space-y-4">
         <Title>{t("titles.contact")}</Title>
         <p className="text-secondary">{description}</p>
+      </div>
+
+      <div className="relative">
+        <Image
+          objectFit="cover"
+          layout="fill"
+          src="/static/Speaker.png"
+          alt="Speaker image"
+          priority={false}
+        />
       </div>
     </section>
   );
