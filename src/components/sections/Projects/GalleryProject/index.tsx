8f5e2b493fb2f69f19c06944461b5a3097c18699
Hiyudev <37001732+Hiyudev@users.noreply.github.com>
2022-07-13 13:13:08 -0300

    Added projects page

---
+++ src/components/sections/Projects/GalleryProject/index.tsx
@@ -0,0 +1,42 @@
+import { useTranslation } from "next-i18next";
+import Image from "next/image";
+import Link from "next/link";
+import { Project } from "../../../../graphql/generated/graphcms";
+
+interface IGalleryProjectProps {
+  projectData: Project;
+}
+
+function GalleryProject({ projectData }: IGalleryProjectProps) {
+  const { t } = useTranslation("project");
+
+  return (
+    <li className="border-secondary group relative overflow-hidden rounded-md border">
+      <Link passHref href={`/project/${projectData.slug}`}>
+        <a
+          aria-label={`${t("datailedProject.label.partOne")} ${
+            projectData.title
+          } ${t("datailedProject.label.partTwo")}.`}
+        >
+          <div className="relative flex h-80 items-center justify-center">
+            {projectData.projectThumbnail && (
+              <Image
+                objectFit="cover"
+                layout="fill"
+                className="scale-100 opacity-75 transition-transform group-hover:scale-105 group-hover:opacity-100"
+                priority={false}
+                alt={projectData.projectThumbnailAlt}
+                src={projectData.projectThumbnail.url}
+              />
+            )}
+            <div className="bg-primary absolute hidden h-32 w-32 items-center justify-center rounded-full group-hover:flex">
+              View project
+            </div>
+          </div>
+        </a>
+      </Link>
+    </li>
+  );
+}
+
+export default GalleryProject;
--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -6,25 +6,34 @@ import {
   Project,
 } from "../../graphql/generated/graphcms";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import Title from "../../components/common/Title";
+import { useTranslation } from "next-i18next";
+import GalleryProject from "../../components/sections/Projects/GalleryProject";
 
 const Footer = dynamic(() => import("../../components/common/Footer"));
 const Layout = dynamic(() => import("../../components/layout"));
 const Navbar = dynamic(() => import("../../components/common/Navbar"));
-
-const ProjectsSection = dynamic(
-  () => import("../../components/sections/Projects")
-);
 
 interface ProjectsPageProps {
   ProjectsData: Project[];
 }
 
 const ProjectsPage = ({ ProjectsData }: ProjectsPageProps) => {
+  const { t } = useTranslation("home");
+
   return (
     <>
       <Navbar />
       <Layout className="mt-20">
-        <ProjectsSection projectsData={ProjectsData ?? []} />
+        <section id="projects" className="mt-8md:mt-16">
+          <Title>{t("titles.projects")}</Title>
+
+          <ul className="mt-4 grid grid-cols-4 gap-2">
+            {ProjectsData.map((project) => (
+              <GalleryProject key={project.slug} projectData={project} />
+            ))}
+          </ul>
+        </section>
       </Layout>
 
       <Footer />
