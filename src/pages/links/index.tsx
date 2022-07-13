a4bcfb0a06058ddcd6b0653273b3b56d0569e9a2
Hiyudev <37001732+Hiyudev@users.noreply.github.com>
2022-07-13 17:59:00 -0300

    Added portfolio directory to the links page.

--- src/pages/links/index.tsx
+++ src/pages/links/index.tsx
@@ -1,4 +1,6 @@
 import Link from "next/link";
+import { GetStaticProps } from "next/types";
+import { User } from "phosphor-react";
 import BigLogoIcon from "../../components/icon/BigLogo";
 import { SocialList } from "../../types";
 
@@ -15,6 +17,16 @@ const LinksPage = () => {
       </div>
 
       <ul className="flex flex-col gap-4">
+        <li>
+          <Link href={"/"} passHref>
+            <a className="bg-primary border-secondary group relative flex min-w-[240px] items-center justify-center gap-2 rounded-full border p-2">
+              <div className="fancy-gradient absolute inset-0.5 -z-10 opacity-0 blur transition-opacity group-hover:opacity-75 group-focus:opacity-75" />
+
+              <User weight="bold" />
+              <span className="text-secondary">Website</span>
+            </a>
+          </Link>
+        </li>
         {Object.entries(SocialList).map(([name, { logo, link }]) => (
           <li key={name}>
             <Link href={link} passHref>
