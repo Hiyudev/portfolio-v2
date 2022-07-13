844e2d7ca8e1e3bb7e48fe70e9aad955ee27b8d2
Hiyudev <37001732+Hiyudev@users.noreply.github.com>
2022-07-13 09:27:37 -0300

    Added anchor to projects page.

--- src/components/sections/Projects/index.tsx
+++ src/components/sections/Projects/index.tsx
@@ -1,4 +1,5 @@
 import { useTranslation } from "next-i18next";
+import Link from "next/link";
 import { Project } from "../../../graphql/generated/graphcms";
 import Title from "../../common/Title";
 import DetailedProject from "./DetailedProject";
@@ -16,7 +17,16 @@ function ProjectsSection({ projectsData, description }: ProjectsSectionProps) {
       <Title>{t("titles.projects")}</Title>
       {description && (
         <p className="text-secondary mt-4 sm:text-lg md:text-xl">
-          {description}
+          {description}{" "}
+          <span>
+            {t("project.visit")}{" "}
+            <Link href="/projects" passHref>
+              <a className="underline-offset-2 transition-colors hover:text-primary-500">
+                {t("project.page")}
+              </a>
+            </Link>
+            .
+          </span>
         </p>
       )}
       <ul className="mt-8 gap-y-5 gap-x-5">
