6a306b06fe8d64b9fb6c16d0c3ac17e919e4b82c
Hiyudev <37001732+Hiyudev@users.noreply.github.com>
2022-07-14 19:28:54 -0300

    Added more SEO tags

--- next-seo.config.js
+++ next-seo.config.js
@@ -1,33 +1,47 @@
 const SEO = {
-  defaultTitle: "Kevin's portfolio",
-  description: "Enthusiast developer passionate about the technology world",
+  defaultTitle: "Kevin Yuki's portfolio",
+  description:
+    "Enthusiast developer passionate about the technology world. I create and design fast and accessible programs to make your idea a real-world product.",
   additionalMetaTags: [
     {
-      property: 'keywords',
-      content: 'kevin, kevin portfolio, hiyudev, hiyudev portfolio',
+      property: "keywords",
+      content:
+        "kevin yuki, kevin yuki portfolio, kevin, kevin portfolio, hiyudev, hiyudev portfolio",
     },
     {
-      name: 'theme_color',
-      content: '#18181b',
-    }
+      name: "theme_color",
+      content: "#ec4899",
+    },
   ],
   additionalLinkTags: [
     {
-      rel: 'icon',
-      href: '/asset/favicon.ico',
-    }
+      rel: "icon",
+      href: "/asset/favicon.ico",
+    },
   ],
   openGraph: {
     type: "website",
     locale: "en_US",
-    site_name: "Kevin's portfolio",
+    url: "https://hiyudev.me/",
+    title: "Kevin Yuki's portfolio",
+    site_name: "Kevin Yuki's portfolio",
+    description:
+      "Enthusiast developer passionate about the technology world. I create and design fast and accessible programs to make your idea a real-world product.",
+    images: [
+      {
+        url: "https://readme.so/asset/banner.png",
+        width: 1360,
+        height: 768,
+        alt: "Og Image Alt",
+      },
+    ],
+    imageWidth: 1360,
+    imageHeight: 768,
   },
   twitter: {
-    handle: "@handle",
-    site: "@site",
+    handle: "@Hiyudev",
     cardType: "summary_large_image",
   },
 };
 
 export default SEO;
-
