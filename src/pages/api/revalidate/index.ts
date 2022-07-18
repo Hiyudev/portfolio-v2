048e05e063542427e67d9e017e0bc7090592abf1
Hiyudev <37001732+Hiyudev@users.noreply.github.com>
2022-07-18 09:03:21 -0300

    feat(api): allow cross-origin api calls

--- src/pages/api/revalidate/index.ts
+++ src/pages/api/revalidate/index.ts
@@ -1,9 +1,27 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { GetAllProjectsDocument } from "../../../graphql/generated/graphcms";
 import { initGraphClient } from "../../../lib/client";
+import Cors from 'cors'
 
+const cors = Cors({
+  methods: ['GET', 'HEAD'],
+})
+
+function runMiddleware(req, res, fn) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result) => {
+      if (result instanceof Error) {
+        return reject(result);
+      }
+
+      return resolve(result);
+    })
+  })
+}
 
 const revalidateHandler = async (req: NextApiRequest, res: NextApiResponse<string>) => {
+  await runMiddleware(req, res, cors);
+
   if (req.query.secret !== process.env.REVALIDATION_SECRET) {
     return res.status(401).send('Invalid token');
   }
