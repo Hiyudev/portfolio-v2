e3888a30eb82205b90bcfb9425eda92a07ab6d3c
Hiyudev <37001732+Hiyudev@users.noreply.github.com>
2022-08-10 15:52:34 -0300

    fix: fix localePath on wrong location

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,9 +1,12 @@
 const path = require("path");
 
+/**
+ * @type {import('next').NextConfig}
+ */
 module.exports = {
   i18n: {
     locales: ["en", "pt"],
     defaultLocale: "en",
-    localePath: path.resolve("./public/locales"),
   },
+  localePath: path.resolve("./public/locales"),
 };
